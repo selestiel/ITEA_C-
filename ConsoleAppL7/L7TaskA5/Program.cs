87fa8cf2c79c64faab3fb8ed5d820ff3728105bf
selestiel <selarch2018@gmail.com>
2020-01-04 15:02:57 +0200

    L7 Home Task Full

--- ConsoleAppL7/L7TaskA5/Program.cs
+++ ConsoleAppL7/L7TaskA5/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace L7TaskA5
 {
@@ -11,7 +7,7 @@ namespace L7TaskA5
         static void Main(string[] args)
         {
             Point A = new Point(4, 4, "A");
-            Point B = new Point(7, 9, "B"); 
+            Point B = new Point(7, 9, "B");
             Point C = new Point(1, 2, "C");
             Figure Tri = new Figure(A, B, C);
             Console.WriteLine("Triangle sides: {0}, {1},  {2}",
@@ -19,7 +15,7 @@ namespace L7TaskA5
                 Tri.side_fig(B, C),
                 Tri.side_fig(A, C));
             Console.WriteLine("Triangle perimeter: {0}",
-                Tri.Perim(Tri.array_points));
+                Tri.Perim(Tri.arr_point));
             Point D = new Point(12, 5, "D");
             Figure Rect = new Figure(A, B, C, D);
             Console.WriteLine("Rectangle sides: {0} , {1}, {2}, {3}",
@@ -28,7 +24,7 @@ namespace L7TaskA5
             Rect.side_fig(C, D),
             Rect.side_fig(A, D));
             Console.WriteLine("Rectangle perimeter: {0}",
-            Rect.Perim(Rect.array_points));
+            Rect.Perim(Rect.arr_point));
             Point E = new Point(4, 4, "A");
             Point F = new Point(8, 8, "B");
             Point G = new Point(-8, -8, "C");
@@ -40,7 +36,7 @@ namespace L7TaskA5
             Quad.side_fig(G, H),
             Quad.side_fig(E, H));
             Console.WriteLine("Quad perimeter: {0}",
-            Quad.Perim(Quad.array_points));
+            Quad.Perim(Quad.arr_point));
 
             Console.ReadKey();
         }
@@ -51,17 +47,17 @@ namespace L7TaskA5
         private int coord_y { get; set; }
         private string coord_name { get; set; }
 
-        public int C_A()
+        public int Get_X()
         {
             int A = coord_x;
             return A;
         }
-        public int C_B()
+        public int Get_Y()
         {
             int B = coord_y;
             return B;
         }
-        public string C_N()
+        public string Get_N()
         {
             string N = coord_name;
             return N;
@@ -74,58 +70,47 @@ namespace L7TaskA5
         }
 
     }
-    class Figure 
+    class Figure
     {
-        public int[][] array_points { get; set; }
+        public Point[] arr_point { get; set; }
         public string array_name { get; set; }
         public double side_fig(Point a, Point b)
         {
-            int x_2 = b.C_A();
-            int x_1 = a.C_A();
-            int y_2 = b.C_B();
-            int y_1 = a.C_B();
+            int x_2 = b.Get_X();
+            int x_1 = a.Get_X();
+            int y_2 = b.Get_Y();
+            int y_1 = a.Get_Y();
             double result = Math.Sqrt(Math.Pow((x_2 - x_1), 2) + Math.Pow((y_2 - y_1), 2));
             return result;
         }
 
-        public double Perim(int[][] array)
+        public double Perim(Point[] array)
         {
             double result = 0;
 
-            for(int i = 0; i< array.Length; i++)
+            for (int i = 0; i < array.Length - 1; i++)
             {
-                for (int j = 0; j < array.Length; j++)
-                {
-                    int x_1 = array[i][j];
-                    int x_2 = array[i + 1][j];
-                    int y_1 = array[i][j + 1];
-                    int y_2 = array[i + 1][j + 1];
-                    result += Math.Sqrt(Math.Pow((x_2 - x_1), 2) + Math.Pow((y_2 - y_1), 2));
-                }
+                int x_1 = array[i].Get_X();
+                int x_2 = array[i + 1].Get_X();
+                int y_1 = array[i].Get_Y();
+                int y_2 = array[i + 1].Get_Y();
+                result += Math.Sqrt(Math.Pow((x_2 - x_1), 2) + Math.Pow((y_2 - y_1), 2));
+
             }
 
             return result;
         }
-        public Figure(Point a,Point b, Point c)
+        public Figure(Point a, Point b, Point c)
         {
-            array_points[0][0] = a.C_A();
-            array_points[0][1] = a.C_B();
-            array_points[1][0] = b.C_A();
-            array_points[1][1] = b.C_B();
-            array_points[2][0] = c.C_A();
-            array_points[2][1] = c.C_B();
+            Point[] array = new Point[3] { a, b, c };
+            arr_point = array;
             array_name = "Triangle";
+
         }
-        public Figure(Point a, Point b, Point c,Point d)
+        public Figure(Point a, Point b, Point c, Point d)
         {
-            array_points[0][0] = a.C_A();
-            array_points[0][1] = a.C_B();
-            array_points[1][0] = b.C_A();
-            array_points[1][1] = b.C_B();
-            array_points[2][0] = c.C_A();
-            array_points[2][1] = c.C_B();
-            array_points[3][0] = d.C_A();
-            array_points[3][1] = d.C_B();
+            Point[] array = new Point[4] { a, b, c, d };
+            arr_point = array;
             array_name = "Rectangle";
         }
     }
