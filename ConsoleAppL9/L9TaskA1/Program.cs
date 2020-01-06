ff1fb7c329a9e1fc432354e2352e2dd5c23b5198
selestiel <selarch2018@gmail.com>
2020-01-06 22:02:11 +0200

    L9HT full

--- ConsoleAppL9/L9TaskA1/Program.cs
+++ ConsoleAppL9/L9TaskA1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace L9TaskA1
 {
@@ -10,40 +6,99 @@ namespace L9TaskA1
     {
         static void Main(string[] args)
         {
-            IPrintable[] array = new IPrintable[10];
-            for(int i=0; i < array.Length; i++)
+            Book B_A = new Book("Bleach");
+            Book B_B = new Book("OnePiece");
+            Book B_C = new Book("MeitanteiConan");
+            Journal J_A = new Journal("Novel");
+            Journal J_B = new Journal("Jump");
+            Journal J_C = new Journal("Webtoon");
+
+            IPrintable[] printable = {
+                B_A,
+                B_B,
+                B_C,
+                J_A,
+                J_B,
+                J_C
+            };
+            for (int i = 0; i < printable.Length; i++)
             {
-                array[i].Print();
+                printable[i].Print();
             }
+            for (int i = 0; i < printable.Length; i++)
+            {
+                
+                B_A.PrintBook(printable[i]);
+                B_B.PrintBook(printable[i]);
+                B_C.PrintBook(printable[i]);
+                J_A.PrintJournal(printable[i]);
+                J_B.PrintJournal(printable[i]);
+                J_C.PrintJournal(printable[i]);
+            }
+
+
+
+            Console.ReadKey();
+
         }
     }
     interface IPrintable
     {
         void Print();
-
+   
     }
     class Book : IPrintable
     {
-        public void Print()
-        {
-            Console.WriteLine("Printing:");
-        }
+        public string Name { get; set; }
+        public string Type = "Book";
 
-    }
-    class Magazine : IPrintable
-    {
-       
-        public void Print()
+        public void PrintBook(IPrintable printable)
         {
-            Console.WriteLine("Printing:");
-
-        }
-        public void Print_mag(IPrintable[] printable)
-        {
-            for(int i=0; i < printable.Length; i++)
+            if (printable is Book)
             {
+                Console.WriteLine("B-Type: {0} B-Name: {1}", Type, Name);
 
             }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine("B-Type: {0} B-Name: {1}", Type, Name);
+        }
+
+        public Book(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class Journal : IPrintable
+    {
+        public string Name { get; set; }
+        public string Type = "Journal";
+
+        public void PrintJournal(IPrintable printable)
+        {
+            if (printable is Journal)
+            {
+                Console.WriteLine("J-Type: {0} J-Name: {1}", Type, Name);
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine("J-Type: {0} J-Name: {1}", Type, Name);
+        }
+        public Journal(string name)
+        {
+            Name = name;
         }
     }
 }
+
