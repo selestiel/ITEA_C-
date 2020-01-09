6d87b486129251b5268a4a6887488cb1d66856b3
selestiel <selarch2018@gmail.com>
2020-01-09 20:38:45 +0200

    L12Tasks upl

--- ConsoleAppL12/L12TaskA1/Program.cs
+++ ConsoleAppL12/L12TaskA1/Program.cs
@@ -10,7 +10,74 @@ namespace L12TaskA1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter param A: ");
+            int I = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter param B: ");
+            int J = Convert.ToInt32(Console.ReadLine());
+            flag:
+            Console.WriteLine("Enter operation: ('*','-','+','/')");
+            char A = Convert.ToChar(Console.ReadLine());
+            int sum = 0;
+            switch (A)
+            {
+                case '*':
+                    sum = Calculator.MUL(I, J);
+                    break;
+                case '/':
+                    try
+                    {
+                        if (J == 0)
+                        {
+                            throw new Exception("DivideByZeroException");
+                        }
+                        sum = Calculator.DIV(I, J);
 
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    
+                    break;
+                case '+':
+                    sum = Calculator.ADD(I, J);
+                    break;
+                case '-':
+                    sum = Calculator.SUB(I, J);
+                    break;
+                default:
+                    Console.WriteLine("Error no existing operation");
+                    goto flag;
+            }
+            Console.WriteLine("Sum is: {0}",sum);
+            Console.ReadKey();
+        }
+    }
+    static class Calculator
+    {
+        static public int SUB(int arg_a, int arg_b)
+        {
+            int sum = arg_a - arg_b;
+            return sum;
+        }
+        static public int ADD(int arg_a, int arg_b)
+        {
+            int sum = arg_a + arg_b;
+            return sum;
+        }
+        static public int MUL(int arg_a, int arg_b)
+        {
+            int sum = arg_a * arg_b;
+            return sum;
+        }
+        static public int DIV(int arg_a, int arg_b)
+        {
+            int sum = arg_a / arg_b;
+            if(arg_b == 0)
+            {
+                Console.WriteLine("ERROR DIVIDE BY ZERO");
+            }
+            return sum;
         }
     }
 }
---
+++ ConsoleAppL12/L12TaskA2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L12TaskA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker[] worker = new Worker[5];
+            for (int i = 0; i < worker.Length; i++)
+            {
+                Console.WriteLine("Enter name and initials: ");
+                worker[i].name = Console.ReadLine();
+                Console.WriteLine("Enter position: ");
+                worker[i].pos = Console.ReadLine();
+                Console.WriteLine("Enter year of duty entrance: ");
+                try
+                {
+                    worker[i].year = Convert.ToDateTime(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Wrong year data.");
+                }
+            }
+            Array.Sort(worker);
+            Console.WriteLine("Enter exp years: ");
+            int exp = Convert.ToInt32(Console.ReadLine());
+            int year = DateTime.Now.Year;
+            for (int i = 0; i < worker.Length; i++)
+            {
+                int w_year = Convert.ToInt32(worker[i].year);
+                if (exp > (year - w_year))
+                {
+                    Console.WriteLine(worker[i].name);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    class Worker
+    {
+        public string name { get; set; }
+        public string pos { get; set; }
+        public DateTime year { get; set; }
+        
+    }
+}
