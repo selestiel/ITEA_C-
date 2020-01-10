43d3f71bd782ba301383a288713a18cb94b1c8eb
selestiel <selarch2018@gmail.com>
2020-01-10 14:28:30 +0200

    L12 task upd

--- ConsoleAppL12/L12TaskA3/Program.cs
+++ ConsoleAppL12/L12TaskA3/Program.cs
@@ -10,6 +10,56 @@ namespace L12TaskA3
     {
         static void Main(string[] args)
         {
+            Price[] prices = new Price[2];
+            for (int i = 0; i < prices.Length; i++)
+            {
+                Console.WriteLine("Enter shop name: ");
+                prices[i].shop_name = Console.ReadLine();
+                Console.WriteLine("Enter item name: ");
+                prices[i].item_name = Console.ReadLine();
+                Console.WriteLine("Enter item price: ");
+                prices[i].item_price = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Choose shop: ");
+            string cshop = Console.ReadLine();
+            GetItem(prices,cshop);
+
+            Console.ReadKey();
         }
+        static void GetItem(Price[] price,string shop)
+        {
+            try
+            {
+                int count = 0;
+                for (int i = 0; i < price.Length; i++)
+                {
+                    if (price[i].shop_name == shop)
+                    {
+                        count++;
+                        price[i].Info();
+                    }
+                }
+                if (count == 0)
+                {
+                    throw new Exception("Item of this shop are not exist!");
+                }
+            }
+            catch (Exception exception) 
+            {
+                Console.WriteLine(exception.Message);
+            } 
+        }
+    }
+    struct Price
+    {
+        public string item_name { get; set; }
+        public string shop_name { get; set; }
+        public int item_price { get; set; }
+
+        public void Info() 
+        {
+            Console.WriteLine("Shop: {0} Item: {1} Price: {2}",shop_name,item_name,item_price);
+        }
+
     }
 }
--- ConsoleAppL12/L12TaskB1/Program.cs
+++ ConsoleAppL12/L12TaskB1/Program.cs
@@ -10,9 +10,39 @@ namespace L12TaskB1
     {
         static void Main(string[] args)
         {
-        
+            MyClassGC Char = new MyClassGC("NPC", 50, 50, 50, 50, 50, 12.5, 12.5, 12.5, 500000);
+            Console.WriteLine(GC.GetTotalMemory(true));
+            Console.WriteLine(GC.MaxGeneration);
+            Console.WriteLine(GC.GetGeneration(Char));
+
             Console.ReadKey();
 
+        }
+        class MyClassGC
+        {
+            string name { get; set; }
+            int strength { get; set; }
+            int vitality { get; set; }
+            int agility { get; set; }
+            int dexterity { get; set; }
+            int wisdom { get; set; }
+            double evasion { get; set; }
+            double accuracy { get; set; }
+            double crit { get; set; }
+            long money { get; set; }
+            public MyClassGC(string n,int str, int vit, int agi, int dex, int wis, double eva, double acc, double cri, long mon)
+            {
+                name = n;
+                strength = str;
+                vitality = vit;
+                agility = agi;
+                dexterity = dex;
+                wisdom = wis;
+                evasion = eva;
+                accuracy = acc;
+                crit = cri;
+                money = mon;
+            }
         }
     }
 }
